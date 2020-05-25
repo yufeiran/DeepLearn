f46548a6a8c23a0bf218c7b7528763d5cd19e2ba
YuFeiran <yufeiran1999@gmail.com>
2020-05-25 23:59:12 +0800

    finish ch4

--- ch04/common/gradient.py
+++ ch04/common/gradient.py
@@ -4,10 +4,13 @@ def numerical_gradient(f,x):
     h=1e-4 #0.0001
     grad=np.zeros_like(x) #生成和x形状相同的数组
 
-    for idx in range(x.size):
+    it=np.nditer(x,flags=['multi_index'],op_flags=['readwrite'])
+
+    while not it.finished:
+        idx=it.multi_index
         tmp_val=x[idx]
         # f(x+h)的计算
-        x[idx]=tmp_val+h
+        x[idx]=float( tmp_val)+h
         fxh1=f(x)
 
         # f(x-h)的计算
@@ -16,4 +19,5 @@ def numerical_gradient(f,x):
 
         grad[idx]=(fxh1-fxh2)/(2*h)
         x[idx]=tmp_val #还原值
+        it.iternext()
     return grad
--- ch04/simpleNet.py
+++ ch04/simpleNet.py
@@ -6,7 +6,7 @@ from common.gradient import numerical_gradient
 
 class simpleNet:
     def __init__(self):
-        self.W=np.random.randn(2,3) #用高斯分布进行初始化
+        self.W=np.random.rand(2,3) #用高斯分布进行初始化
     
     def predict(self,x):
         return np.dot(x,self.W)
@@ -17,3 +17,18 @@ class simpleNet:
         loss=cross_entropy_error(y,t)
         return loss 
 
+
+net=simpleNet()
+print(net.W)
+x=np.array([0.6,0.9])
+p=net.predict(x)
+print(p)
+t=np.array([0,0,0])
+t[np.argmax(p)]=1
+print(net.loss(x,t))
+
+def f(W):
+    return net.loss(x,t)
+
+dW=numerical_gradient(f,net.W)
+print(dW)

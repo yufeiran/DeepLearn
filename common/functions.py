eff10e1114e1e648eb9fdb0acf69a564b92ae78f
YuFeiran <yufeiran1999@gmail.com>
2020-05-27 15:53:22 +0800

    ch05 finish!

--- common/functions.py
+++ common/functions.py
@@ -14,9 +14,12 @@ def cross_entropy_error(y,t):
     if y.ndim==1:
         t=t.reshape(1,t.size)
         y=y.reshape(1,y.size)
+    # ?????one-hot-vector???????????????
+    if t.size==y.size:
+        t=t.argmax(axis=1)
 
     batch_size=y.shape[0]
-    return -np.sum(t*np.log(y+1e-7))/batch_size
+    return -np.sum(np.log(y[np.arange(batch_size),t]+1e-7))/batch_size
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
